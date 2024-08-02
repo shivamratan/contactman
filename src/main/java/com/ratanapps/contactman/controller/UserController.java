dc613e7784550a880c567550674fe60cba2ab8f6
shivamratan <shivamratan61@gmail.com>
2024-08-02 14:15:06 +0530

    Add Contact design

--- src/main/java/com/ratanapps/contactman/controller/UserController.java
+++ src/main/java/com/ratanapps/contactman/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.ratanapps.contactman.controller;
 
+import com.ratanapps.contactman.entity.Contact;
 import com.ratanapps.contactman.entity.User;
 import com.ratanapps.contactman.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -17,20 +20,31 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+
+    // Adding common data for every handler
+    @ModelAttribute
+    public void addCommonData(Model m, Principal principal) {
+
+        String userName = principal.getName();
+        User user = userService.getUserByUserEmail(userName);
+
+        m.addAttribute("user", user);
+    }
+
     @RequestMapping(value = "/index", method = RequestMethod.GET)
     public String dashboard(Model model, Principal principal) {
 
-
-        String userName = principal.getName();
-        System.out.println("USERNAME "+userName);
-
-        User user = userService.getUserByUserEmail(userName);
-        System.out.println("USER "+user);
-
         model.addAttribute("message", "Sample message");
-        model.addAttribute("user", user);
 
         return "general/user_dashboard";
+    }
+
+    // open add Contact form handler
+    @GetMapping("/add-contact")
+    public String openAddContactForm(Model model) {
+        model.addAttribute("title","Add Contact - ContactMan");
+        model.addAttribute("contact", new Contact());
+        return "general/add_contact_form";
     }
 
 
